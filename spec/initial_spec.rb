b9724a7f628f5577cfa214d4249b2f102f123d56
Marin Jankovski <marin@gitlab.com>
2013-10-28 13:17:29 +0100

    First test.

--- spec/initial_spec.rb
+++ spec/initial_spec.rb
@@ -9,10 +9,14 @@ describe "gitlab::initial" do
     stub_command("git --version | grep 1.7.12.4").and_return(true)
   end
 
-  it "installs all default packages" do
-    packages = chef_run.node['gitlab']['packages']
-    packages.each do |pkg|
-      expect(chef_run).to install_package(pkg)
+  describe "under ubuntu 12.04" do
+    let(:chef_run) { ChefSpec::Runner.new(platform: "ubuntu", version: "12.04").converge("gitlab::initial") }
+
+    it "installs all default packages" do
+      packages = chef_run.node['gitlab']['packages']
+      packages.each do |pkg|
+        expect(chef_run).to install_package(pkg)
+      end
     end
   end
 end
