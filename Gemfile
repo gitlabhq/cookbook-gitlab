dc2ed8070a87e252417e17cb7ec91058f5f7094f
Marin Jankovski <marin@gitlab.com>
2013-10-28 13:14:21 +0100

    First test.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'berkshelf', "2.0.10"
-gem 'test-kitchen', "1.0.0.beta.3", group: :integration
-gem 'kitchen-vagrant', group: :integration
 gem 'foodcritic'
 gem 'chefspec', "3.0.1"
---
+++ spec/initial_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "gitlab::initial" do
+  let(:chef_run) { ChefSpec::Runner.new.converge("gitlab::initial") }
+
+  before do
+    # stubbing git commands because initial recipe requires gitlab::git
+    stub_command("test -f /var/chef/cache/git-1.7.12.4.zip").and_return(true)
+    stub_command("git --version | grep 1.7.12.4").and_return(true)
+  end
+
+  it "installs all default packages" do
+    packages = chef_run.node['gitlab']['packages']
+    packages.each do |pkg|
+      expect(chef_run).to install_package(pkg)
+    end
+  end
+end
