34b1fb394e7136e0fb4ce657b5e31201c3793f43
Marin Jankovski <marin@gitlab.com>
2013-10-29 18:43:18 +0100

    Use file resource, bash commands should run only once.

--- attributes/production.rb
+++ attributes/production.rb
@@ -1,5 +1,5 @@
 # User
-default['gitlab']['production']['user'] = "git"
+default['gitlab']['production']['user'] = "git" # Changing default user may have unintended consequences. Update init script if you change this 
 default['gitlab']['production']['group'] = "git"
 default['gitlab']['production']['home'] = "/home/git"
 
@@ -8,7 +8,7 @@ default['gitlab']['production']['shell_path'] = "/home/git/gitlab-shell"
 
 # GitLab hq
 default['gitlab']['production']['revision'] = "6-2-stable"
-default['gitlab']['production']['path'] = "/home/git/gitlab"
+default['gitlab']['production']['path'] = "/home/git/gitlab" # Changing default path may have unintended consequences. Update init script if you change this
 
 # GitLab shell config
 default['gitlab']['production']['repos_path'] = "/home/git/repositories"
