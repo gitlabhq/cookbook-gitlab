015a7ce4b486ac945983e6dea55f63708601a63c
Marin Jankovski <marin@gitlab.com>
2013-10-29 15:11:04 +0100

    Add rspec config to helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,23 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  # Specify the path for Chef Solo to find cookbooks (default: [inferred from
+  # the location of the calling spec file])
+  # config.cookbook_path = '/var/cookbooks'
+
+  # Specify the path for Chef Solo to find roles (default: [ascending search])
+  # config.role_path = '/var/roles'
+
+  # Specify the Chef log_level (default: :warn)
+  config.log_level = :error
+
+  # Specify the path to a local JSON file with Ohai data (default: nil)
+  # config.path = 'ohai.json'
+
+  # Specify the operating platform to mock Ohai data from (default: nil)
+  config.platform = 'ubuntu'
+
+  # Specify the operating version to mock Ohai data from (default: nil)
+  config.version = '12.04'
+end
